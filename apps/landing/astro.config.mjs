2ca2420ae93eb43335571d4fc2943da4e6076657
Miguel Rengifo <miguelreng@gmail.com>
2026-05-26 01:57:29 -0500

    Improve bookmark extension feedback

--- apps/landing/astro.config.mjs
+++ apps/landing/astro.config.mjs
@@ -2,6 +2,6 @@ import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: process.env.SITE_URL ?? "https://dragonfruit.sh",
+  site: process.env.SITE_URL || "https://dragonfruit.sh",
   integrations: [sitemap()],
 });
